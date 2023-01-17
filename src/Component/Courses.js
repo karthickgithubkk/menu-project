267b6010bd73c3159ed923cb5deb55dd97f1a195
karthick kannan <kannankund321@gmail.com>
2023-01-17 20:40:30 +0530

    added

--- src/Component/Courses.js
+++ src/Component/Courses.js
@@ -11,7 +11,7 @@ const Courses = ({courses}) => {
 <div className="item-info">
     <header>
         <h4>{title}</h4>
-        <h4 className="price">{price} </h4>
+        <h4 className="price">₹ {price} </h4>
         </header>
         <p className="item-text">{desc}</p>
 </div>
